ff53bd79a1762d82b7050de722e8973270f84150
schroscatt <56359873+schroscatt@users.noreply.github.com>
2020-07-23 00:12:27 +0300

    Add files via upload

---
+++ ML.py
@@ -0,0 +1,56 @@
+
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeRegressor
+import sys
+
+iowa_file_path = '../input/train.csv'
+
+home_data = pd.read_csv(iowa_file_path)
+# Create target object and call it y
+y = home_data.SalePrice
+# Create X
+features = ['LotArea', 'YearBuilt', '1stFlrSF', '2ndFlrSF', 'FullBath', 'BedroomAbvGr', 'TotRmsAbvGrd']
+X = home_data[features]
+
+# Split into validation and training data
+train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=1)
+
+# Specify Model
+iowa_model = DecisionTreeRegressor(random_state=1)
+# Fit Model
+iowa_model.fit(train_X, train_y)
+
+# Make validation predictions and calculate mean absolute error
+val_predictions = iowa_model.predict(val_X)
+val_mae = mean_absolute_error(val_predictions, val_y)
+print("Validation MAE when not specifying max_leaf_nodes: {:,.0f}".format(val_mae))
+def get_mea(max_nodes,train_X,val_X,train_y,val_y):
+    model = DecisionTreeRegressor(max_leaf_nodes= max_nodes, random_state=1)
+    model.fit(train_X,train_y)
+    mae = mean_absolute_error(val_y, model.predict(val_X))
+    return mae
+
+mini = sys.maxsize
+maxi = 0
+for nodeL in [100,500,1000,5000]:
+    mae = get_mea(nodeL,train_X,val_X,train_y,val_y)
+    if mae<mini:
+        mini = mae
+        maxi = nodeL
+# Using best value for max_leaf_nodes
+iowa_model = DecisionTreeRegressor(max_leaf_nodes=maxi, random_state=1)
+iowa_model.fit(train_X, train_y)
+val_predictions = iowa_model.predict(val_X)
+val_mae = mean_absolute_error(val_predictions, val_y)
+print("Validation MAE for best value of max_leaf_nodes: {:,.0f}".format(val_mae))
+
+# Define the model. Set random_state to 1
+rf_model = RandomForestRegressor(random_state=1)
+rf_model.fit(train_X, train_y)
+rf_val_predictions = rf_model.predict(val_X)
+rf_val_mae = mean_absolute_error(rf_val_predictions, val_y)
+
+print("Validation MAE for Random Forest Model: {:,.0f}".format(rf_val_mae))
